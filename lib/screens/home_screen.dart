8bdc6795a2d1389dc64bb289293406980c24b61d
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-08-11 13:43:47 +0545

    Curve Clipers Added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'url_qr_screen.dart';
-import 'wifi_qr_screen.dart';
+
+import '../screens/url_qr_screen.dart';
+import '../screens/widgets/curve_clippers.dart';
+import '../screens/wifi_qr_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -130,69 +132,69 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class TopCurveClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    final path = Path();
-    path.lineTo(0, size.height * 0.375);
+// class TopCurveClipper extends CustomClipper<Path> {
+//   @override
+//   Path getClip(Size size) {
+//     final path = Path();
+//     path.lineTo(0, size.height * 0.375);
 
-    final firstControlPoint = Offset(size.width * 0.25, size.height * 0.585);
-    final firstEndPoint = Offset(size.width * 0.5, size.height * 0.475);
-    path.quadraticBezierTo(
-      firstControlPoint.dx,
-      firstControlPoint.dy,
-      firstEndPoint.dx,
-      firstEndPoint.dy,
-    );
+//     final firstControlPoint = Offset(size.width * 0.25, size.height * 0.585);
+//     final firstEndPoint = Offset(size.width * 0.5, size.height * 0.475);
+//     path.quadraticBezierTo(
+//       firstControlPoint.dx,
+//       firstControlPoint.dy,
+//       firstEndPoint.dx,
+//       firstEndPoint.dy,
+//     );
 
-    final secondControlPoint = Offset(size.width * 0.75, size.height * 0.365);
-    final secondEndPoint = Offset(size.width, size.height * 0.575);
-    path.quadraticBezierTo(
-      secondControlPoint.dx,
-      secondControlPoint.dy,
-      secondEndPoint.dx,
-      secondEndPoint.dy,
-    );
+//     final secondControlPoint = Offset(size.width * 0.75, size.height * 0.365);
+//     final secondEndPoint = Offset(size.width, size.height * 0.575);
+//     path.quadraticBezierTo(
+//       secondControlPoint.dx,
+//       secondControlPoint.dy,
+//       secondEndPoint.dx,
+//       secondEndPoint.dy,
+//     );
 
-    path.lineTo(size.width, 0);
-    path.close();
-    return path;
-  }
+//     path.lineTo(size.width, 0);
+//     path.close();
+//     return path;
+//   }
 
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
-}
+//   @override
+//   bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+// }
 
-class BottomCurveClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    final path = Path();
-    path.moveTo(0, size.height * 0.225);
+// class BottomCurveClipper extends CustomClipper<Path> {
+//   @override
+//   Path getClip(Size size) {
+//     final path = Path();
+//     path.moveTo(0, size.height * 0.225);
 
-    final firstControlPoint = Offset(size.width * 0.25, size.height * 0.1);
-    final firstEndPoint = Offset(size.width * 0.5, size.height * 0.25);
-    path.quadraticBezierTo(
-      firstControlPoint.dx,
-      firstControlPoint.dy,
-      firstEndPoint.dx,
-      firstEndPoint.dy,
-    );
+//     final firstControlPoint = Offset(size.width * 0.25, size.height * 0.1);
+//     final firstEndPoint = Offset(size.width * 0.5, size.height * 0.25);
+//     path.quadraticBezierTo(
+//       firstControlPoint.dx,
+//       firstControlPoint.dy,
+//       firstEndPoint.dx,
+//       firstEndPoint.dy,
+//     );
 
-    final secondControlPoint = Offset(size.width * 0.75, size.height * 0.135);
-    final secondEndPoint = Offset(size.width, size.height * 0.125);
-    path.quadraticBezierTo(
-      secondControlPoint.dx,
-      secondControlPoint.dy,
-      secondEndPoint.dx,
-      secondEndPoint.dy,
-    );
+//     final secondControlPoint = Offset(size.width * 0.75, size.height * 0.135);
+//     final secondEndPoint = Offset(size.width, size.height * 0.125);
+//     path.quadraticBezierTo(
+//       secondControlPoint.dx,
+//       secondControlPoint.dy,
+//       secondEndPoint.dx,
+//       secondEndPoint.dy,
+//     );
 
-    path.lineTo(size.width, size.height);
-    path.lineTo(0, size.height);
-    path.close();
-    return path;
-  }
+//     path.lineTo(size.width, size.height);
+//     path.lineTo(0, size.height);
+//     path.close();
+//     return path;
+//   }
 
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
-}
+//   @override
+//   bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+// }
---
+++ lib/screens/widgets/curve_clippers.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class TopCurveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.lineTo(0, size.height * 0.375);
+
+    final firstControlPoint = Offset(size.width * 0.25, size.height * 0.585);
+    final firstEndPoint = Offset(size.width * 0.5, size.height * 0.475);
+    path.quadraticBezierTo(
+      firstControlPoint.dx,
+      firstControlPoint.dy,
+      firstEndPoint.dx,
+      firstEndPoint.dy,
+    );
+
+    final secondControlPoint = Offset(size.width * 0.75, size.height * 0.365);
+    final secondEndPoint = Offset(size.width, size.height * 0.575);
+    path.quadraticBezierTo(
+      secondControlPoint.dx,
+      secondControlPoint.dy,
+      secondEndPoint.dx,
+      secondEndPoint.dy,
+    );
+
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+class BottomCurveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    path.moveTo(0, size.height * 0.225);
+
+    final firstControlPoint = Offset(size.width * 0.25, size.height * 0.1);
+    final firstEndPoint = Offset(size.width * 0.5, size.height * 0.25);
+    path.quadraticBezierTo(
+      firstControlPoint.dx,
+      firstControlPoint.dy,
+      firstEndPoint.dx,
+      firstEndPoint.dy,
+    );
+
+    final secondControlPoint = Offset(size.width * 0.75, size.height * 0.135);
+    final secondEndPoint = Offset(size.width, size.height * 0.125);
+    path.quadraticBezierTo(
+      secondControlPoint.dx,
+      secondControlPoint.dy,
+      secondEndPoint.dx,
+      secondEndPoint.dy,
+    );
+
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
