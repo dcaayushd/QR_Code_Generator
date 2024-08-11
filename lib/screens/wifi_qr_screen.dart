eefd21f4f0cb5ff5af7b9adf3d49ee20da935a10
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-08-11 13:45:37 +0545

    Generates Valid WIFI QR Only

--- lib/screens/wifi_qr_screen.dart
+++ lib/screens/wifi_qr_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import '../screens/widgets/curve_clippers.dart';
 import '../utils/qr_generator.dart';
 import '../widgets/qr_display.dart';
 
@@ -13,51 +15,177 @@ class WifiQrScreenState extends State<WifiQrScreen> {
   final _ssidController = TextEditingController();
   final _passwordController = TextEditingController();
   String? _qrData;
+  String? _errorMessage;
+
+  bool _isValidWifi() {
+    return _ssidController.text.isNotEmpty &&
+        _passwordController.text.isNotEmpty;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
         title: const Text(
           'WiFi QR Code',
+          style: TextStyle(color: Colors.white),
         ),
+        iconTheme: const IconThemeData(color: Colors.white),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _ssidController,
-              decoration: const InputDecoration(
-                labelText: 'Enter WiFi SSID',
+      body: Stack(
+        children: [
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: ClipPath(
+              clipper: TopCurveClipper(),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.4,
+                color: const Color(0xFF2A2D3E),
               ),
             ),
-            const SizedBox(height: 10),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Enter WiFi Password',
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: ClipPath(
+              clipper: BottomCurveClipper(),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.7,
+                color: Colors.white,
               ),
-              obscureText: true,
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                setState(
-                  () {
-                    _qrData = QrGenerator.getWifiString(
-                      ssid: _ssidController.text,
-                      password: _passwordController.text,
-                    );
-                  },
-                );
-              },
-              child: const Text('Generate QR Code'),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).size.height * 0.25,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.2),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        controller: _ssidController,
+                        decoration: InputDecoration(
+                          labelText: 'Enter WiFi SSID',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide:
+                                const BorderSide(color: Color(0xFF2A2D3E)),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.2),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: TextField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          labelText: 'Enter WiFi Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide:
+                                const BorderSide(color: Color(0xFF2A2D3E)),
+                          ),
+                        ),
+                        obscureText: true,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_isValidWifi()) {
+                          setState(() {
+                            _qrData = QrGenerator.getWifiString(
+                              ssid: _ssidController.text,
+                              password: _passwordController.text,
+                            );
+                            _errorMessage = null;
+                          });
+                        } else {
+                          setState(() {
+                            _qrData = null;
+                            _errorMessage =
+                                'Please enter both SSID and password';
+                          });
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF2A2D3E),
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 15),
+                        minimumSize:
+                            Size(MediaQuery.of(context).size.width * 0.8, 70),
+                      ),
+                      child: const Text('Generate QR Code',
+                          style: TextStyle(fontSize: 16)),
+                    ),
+                    const SizedBox(height: 20),
+                    if (_errorMessage != null)
+                      Text(
+                        _errorMessage!,
+                        style: const TextStyle(color: Colors.red),
+                      ),
+                    if (_qrData != null) QrDisplay(data: _qrData!),
+                  ],
+                ),
+              ),
             ),
-            const SizedBox(height: 20),
-            if (_qrData != null) QrDisplay(data: _qrData!),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
