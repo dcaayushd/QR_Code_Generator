d16193bcee200127391fb7bf3b1db78d0a19e090
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-08-12 20:11:35 +0545

    QR Display Updated

--- lib/widgets/qr_display.dart
+++ lib/widgets/qr_display.dart
@@ -3,18 +3,20 @@ import 'package:qr_flutter/qr_flutter.dart';
 
 class QrDisplay extends StatelessWidget {
   final String data;
+  final Size size;
 
   const QrDisplay({
     super.key,
     required this.data,
-  });
+    Size? size,
+  }) : size = size ?? const Size(250, 250);
 
   @override
   Widget build(BuildContext context) {
     return QrImageView(
       data: data,
       version: QrVersions.auto,
-      size: 200.0,
+      size: size.width,
     );
   }
 }
