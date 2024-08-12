ae11a43d4f9d136cff1615103699c371b459e8e1
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-08-12 20:08:52 +0545

    Snackbar Updated

--- lib/widgets/custom_snackbar.dart
+++ lib/widgets/custom_snackbar.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
 
 class CustomSnackBar {
-  static void show(BuildContext context, String message) {
+  static void show(
+      BuildContext context, String message, Color backgroundColor) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text(message),
+        content: Center(
+          child: Text(
+            message,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              fontSize: 16.0,
+            ),
+          ),
+        ),
         duration: const Duration(milliseconds: 500),
-        backgroundColor: Colors.orange,
-        behavior: SnackBarBehavior.floating,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        margin: const EdgeInsets.only(
-          bottom: 20,
-          left: 20,
-          right: 20,
-        ),
+        backgroundColor: backgroundColor,
+        behavior: SnackBarBehavior.fixed,
       ),
     );
   }
